9c290cbc220b77f110a9e36ff0062cc93f0940ad
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2020-07-22 22:00:35 +0530

    added post service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [],
   controllers: [AppController, PostsController],
-  providers: [AppService],
+  providers: [AppService, PostService],
 })
 export class AppModule {}
