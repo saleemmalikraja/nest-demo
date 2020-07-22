9b726d24b9b733776b23159ee15f39202465202d
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2020-07-22 22:03:38 +0530

    added basic posts and its verbos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
-import { PostService } from './post/post.service';
+import { PostsService } from './posts/posts.service';
 
 @Module({
   imports: [],
   controllers: [AppController, PostsController],
-  providers: [AppService, PostService],
+  providers: [AppService, PostService, PostsService],
 })
 export class AppModule {}
